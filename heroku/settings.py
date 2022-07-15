5b565d5daa6667d73e43dc521a16d29d4f5ee7fc
abdullahakram2018 <37751853+abdullahakram2018@users.noreply.github.com>
2022-07-15 20:52:01 +0300

    satic

--- heroku/settings.py
+++ heroku/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)h31x6poyqhzla%&%^)$3!w#^3ov+_4a)t!kzmxjau_(4-)th8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','abdullah2022.herokuapp.com']
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
